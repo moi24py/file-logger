0cf5312bd11608e4e19c4f053e94cb19bfb3429e
moi24py <moi24py@gmail.com>
2025-08-17 16:27:39 +0200

    use snprintf() to format datetime string

--- data/generate_sensor_inputs.c
+++ data/generate_sensor_inputs.c
@@ -3,22 +3,44 @@
 #include <stdio.h>
 #include <time.h>
 
-void print_datetime();
+int formatted_datetime(char *buffer);
 
 int main() {
-    print_datetime();
+    char str[20];
+    formatted_datetime(str);
     return 0;
 }
 
-void print_datetime(){
+int formatted_datetime(char *buffer){
     /* Get date and time, supposing the system has an initialized system clock.
     The date and time have been set via a hardware Real-Time Clock or Network Time Protocol sync */
-
     time_t rawdt = time(NULL); // get timelapse in seconds starting from 1/1/1970 until now
     struct tm *now = localtime(&rawdt); // convert the seconds in years, months, days, hours, minutes, seconds
 
-    // print the current date and time (YYYY-MM-DD HH:MM:SS)
-    printf("%04d-%02d-%02d %02d:%02d:%02d\n",
-           now->tm_year + 1900, now->tm_mon + 1, now->tm_mday,
-           now->tm_hour, now->tm_min, now->tm_sec);
+    /* Print the current date and time (YYYY-MM-DD HH:MM:SS) */
+    //printf("%04d-%02d-%02d %02d:%02d:%02d\n",
+    //       now->tm_year + 1900, now->tm_mon + 1, now->tm_mday,
+    //       now->tm_hour, now->tm_min, now->tm_sec);
+    
+    /* If current date and time have not been retrieved */
+    if (!now){
+        perror("Error in retrieving current date and time");
+        return 1;
+    }
+
+    /* Create a formatted string with current date and time (YYYY-MM-DD HH:MM:SS) */
+    int written_bytes = snprintf(buffer, sizeof(buffer), "%04d-%02d-%02d %02d:%02d:%02d",
+                                  now->tm_year + 1900, now->tm_mon + 1, now->tm_mday,
+                                  now->tm_hour, now->tm_min, now->tm_sec);
+    
+    /* If snprintf() had an error, returned an empty string, or a truncated string */
+    if (written_bytes < 0 || written_bytes >= sizeof(buffer)) {
+        perror("Error in generating the formatted string");
+        return 1;
+    }
+    
+    /* DEBUG: print formatted string */
+    printf("%s\n", buffer);
+
+    return 0;
 }
